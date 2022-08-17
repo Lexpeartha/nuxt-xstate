5543f7f9e4eef738010ba70c1b152f7a16c991f4
Lexpeartha <sevic.official@gmail.com>
2022-08-18 00:51:41 +0200

    test: setup vitest and add basic e2e tests

---
+++ tests/e2e/ssr.test.ts
@@ -0,0 +1,34 @@
+/**
+ * @vitest-environment node
+ */
+import { fileURLToPath } from 'node:url'
+import { describe, test, expect } from 'vitest'
+import { setup, createPage } from '@nuxt/test-utils-edge'
+
+describe('ssr', async () => {
+  await setup({
+    server: true,
+    browser: true,
+    rootDir: fileURLToPath(new URL('../fixture', import.meta.url))
+  })
+
+  test('shows proper state before clicking', async () => {
+    const page = await createPage('/')
+
+    const stateText = await page.innerText('[data-test-id="state-txt"]')
+    expect(stateText).toBe('idle')
+  })
+
+  test('shows proper state after clicking', async () => {
+    const page = await createPage('/')
+
+    await page.click('[data-test-id="click-btn"]')
+
+    const stateText = await page.innerText('[data-test-id="state"]')
+    expect(stateText).toBe('loading')
+  })
+
+  // test('auto-imports state machine correctly', () => {
+  //   expect(1 + 1).toBe(2)
+  // })
+})
---
+++ tests/fixture/nuxt.config.ts
@@ -0,0 +1,9 @@
+import { defineNuxtConfig } from 'nuxt'
+import XStateModule from '../../'
+
+export default defineNuxtConfig({
+  modules: [
+    // @ts-ignore-next-line
+    XStateModule
+  ]
+})
