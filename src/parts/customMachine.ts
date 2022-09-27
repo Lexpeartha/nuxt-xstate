8b3176f353f1dfd81c478f9c116c297b33c7fec8
Daniel Roe <daniel@roe.dev>
2022-09-27 13:18:33 +0100

    fix: use `imports.extend` to implement suffix (#18)

--- src/parts/customMachine.ts
+++ src/parts/customMachine.ts
@@ -1,5 +1,4 @@
-import fs from 'fs/promises'
-import { useNuxt, useLogger, addTemplate, addImportsDir } from '@nuxt/kit'
+import { useNuxt, addTemplate, addImportsDir } from '@nuxt/kit'
 import { globby } from 'globby'
 import { resolve } from 'pathe'
 
@@ -8,101 +7,24 @@ import type { CustomMachinesOptions } from '../types'
 export const setupCustomMachines = async (machinesOptions: CustomMachinesOptions) => {
   const nuxt = useNuxt()
 
-  const nuxtSrcDir = nuxt.options.srcDir
-  const nuxtBuildDir = nuxt.options.buildDir
   const { dir, importSuffix } = machinesOptions
 
-  const resolvedDir = resolve(nuxtSrcDir, dir)
-  const resolvedBuildDir = resolve(nuxtBuildDir, 'xstate')
+  const resolvedDir = resolve(nuxt.options.srcDir, dir)
 
-  await handleAutoImport({
-    resolvedDir,
-    resolvedBuildDir,
-    importSuffix,
-    moduleStarting: true
-  })
+  addImportsDir(resolvedDir)
 
-  addImportsDir(resolvedBuildDir)
+  nuxt.hook('imports:extend', (imports) => {
+    for (const i of imports) {
+      const file = i.from
+      if (!file.startsWith(resolvedDir)) { continue }
 
-  nuxt.hook('builder:watch', async (_, path) => {
-    const machineDirAffected = resolve(nuxtSrcDir, path).includes(resolvedDir)
-    const isJsOrTs = path.endsWith('.js') || path.endsWith('.ts')
+      // Update import name with import suffix
+      const fullFileName = file.split('/').at(-1)
+      const bareName = fullFileName.split('.').at(0).toLowerCase()
 
-    if (machineDirAffected && isJsOrTs) {
-      await handleAutoImport({ resolvedDir, resolvedBuildDir, importSuffix })
+      const newName = bareName + importSuffix
+
+      i.as = newName
     }
   })
-}
-
-const handleAutoImport = async (options: {
-  resolvedDir: string,
-  resolvedBuildDir: string,
-  importSuffix: string,
-  moduleStarting?: boolean
-}) => {
-  const logger = useLogger('nuxt-xstate')
-  const { resolvedDir, resolvedBuildDir, importSuffix, moduleStarting = false } = options
-
-  logger.info(
-    moduleStarting
-      ? 'Custom state machine importing is enabled, setting up auto-imports...'
-      : 'Change in the state machine directory detected, updating imports...'
-  )
-  const startTime = Date.now()
-
-  // Remove all previous files
-  await deleteMachineImportFiles(resolvedBuildDir)
-  // Generate new ones
-  await updateMachineImportsTemplates(resolvedDir, importSuffix)
-
-  const elapsedTime = Date.now() - startTime
-  logger.info(
-    `Finished ${moduleStarting ? 'creating' : 'updating'} imports for state machines in ${elapsedTime}ms`
-  )
-}
-
-const scanMachines = async (resolvedDir: string) => {
-  // Sorting to make it consistent between runs
-  return (await globby(resolvedDir)).sort()
-}
-
-const deleteMachineImportFiles = async (resolvedBuildDir: string) => {
-  let errorOccurred = false
-
-  const files = await fs.readdir(resolvedBuildDir).catch((_) => {
-    errorOccurred = true
-  })
-
-  if (errorOccurred || !files) { return }
-
-  const fileDeletionPromises = files.map(f => fs.unlink(resolve(resolvedBuildDir, f)))
-
-  await Promise.all(fileDeletionPromises)
-}
-
-const updateMachineImportsTemplates = async (resolvedDir: string, importSuffix: string) => {
-  const scannedFiles = await scanMachines(resolvedDir)
-
-  for (const file of scannedFiles) {
-    const fullFileLocation = file.split('/').slice(0, -1).join('/')
-    const fullFileName = file.split('/').at(-1)
-    const bareName = fullFileName.split('.').at(0).toLowerCase()
-
-    const newName = bareName + importSuffix
-    const location = fullFileLocation + '/' + bareName
-
-    addTemplate({
-      write: true,
-      filename: `xstate/${newName}.ts`,
-      getContents: () => {
-        return `export { default } from '${location}'`
-      }
-    })
-    // addTemplate({
-    //   filename: `xstate/${newName}.d.ts`,
-    //   getContents: () => {
-    //     return `export {} \ndeclare global { const ${newName}: typeof import('${location}')['default'] }`
-    //   }
-    // })
-  }
 }
