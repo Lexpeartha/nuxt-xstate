0b0e673357376003dd6245760d5b00d2848dfa1b
Aleksa <sevic.official@gmail.com>
2022-10-03 21:10:53 +0200

    chore: update module options

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,6 +4,33 @@ export interface CustomMachinesOptions {
 }
 
 export interface ModuleOptions {
-  minimal: boolean;
-  customMachines: CustomMachinesOptions | false;
+  /**
+   * Whether to use minimal xstate implementation
+   *
+   * @default false
+   */
+  minimal: boolean,
+  /**
+   * Custom machines options
+   *
+   * @default { dir: 'machines', importSuffix: 'Machine' }
+   */
+  customMachines: CustomMachinesOptions | false,
+  /**
+   * Array of auto imports from xstate to be available in your app.
+   *
+   * @default ['createMachine']
+   *
+   * @example
+   * ```js
+   * autoImports: [
+   *  // automatically import `createMachine`
+   *  'createMachine',
+   *  // automatically import `createMachine` as `createXStateMachine`
+   *  ['createMachine', 'createXStateMachine']',
+   * ]
+   * ```
+   *
+   */
+  autoImports: Array<string | [string, string]>,
 }
